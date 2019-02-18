4ab82527a44cd89448907796e94dbb18320b0a90
云却 <479813005@qq.com>
2019-02-18 23:11:47 +0900

    添加KCP引用

---
+++ Megumin.Remote/KcpRemote.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets.Kcp;
+using System.Text;
+
+namespace Megumin.Remote
+{
+    class KcpRemote
+    {
+        public Kcp kcp = new Kcp(1,null);
+    }
+}
