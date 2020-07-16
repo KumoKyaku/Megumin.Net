96dd590ea373fbff2e6734a659c32ca405555b03
云却 <479813005@qq.com>
2020-07-17 00:56:17 +0800

    注释掉暂时不用函数

--- DemoServer/GateService.cs
+++ DemoServer/GateService.cs
@@ -32,24 +32,24 @@ namespace DemoServer
 
         class GateRemote : TcpRemote
         {
-            protected async override ValueTask<object> OnReceive(object message)
-            {
-                switch (message)
-                {
-                    case string str:
-                        return $"{str} world";
-                    case Login2Gate login:
+            //protected async override ValueTask<object> OnReceive(object message)
+            //{
+            //    switch (message)
+            //    {
+            //        case string str:
+            //            return $"{str} world";
+            //        case Login2Gate login:
 
-                        Console.WriteLine($"客户端登陆请求：{login.Account}-----{login.Password}");
+            //            Console.WriteLine($"客户端登陆请求：{login.Account}-----{login.Password}");
 
-                        Login2GateResult resp = new Login2GateResult();
-                        resp.IsSuccess = true;
-                        return resp;
-                    default:
-                        break;
-                }
-                return null;
-            }
+            //            Login2GateResult resp = new Login2GateResult();
+            //            resp.IsSuccess = true;
+            //            return resp;
+            //        default:
+            //            break;
+            //    }
+            //    return null;
+            //}
         }
 
         public void Update(double deltaTime)
