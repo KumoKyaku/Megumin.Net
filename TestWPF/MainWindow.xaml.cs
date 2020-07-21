57678c63946e5bbbab37ae8fc0907cab2b474889
云却 <479813005@qq.com>
2020-07-21 23:12:40 +0800

    增加测试APP

--- TestWPF/MainWindow.xaml.cs
+++ TestWPF/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using Megumin.Remote;
 using Megumin.Remote.Echo;
+using Megumin.Remote.Test;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TestWPF;
 
 namespace TestWPF
 {
@@ -22,14 +26,19 @@ namespace TestWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TestRemote client;
+        private EchoTcp server;
+
         public MainWindow()
         {
             InitializeComponent();
+            MessageLUT.Regist(new TestPacket1());
+            MessageLUT.Regist(new TestPacket2());
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -39,16 +48,69 @@ namespace TestWPF
             this.监听状态.Content = $"开始监听";
         }
 
-        private static async void Listen(TcpRemoteListener remote)
+        private async void Listen(TcpRemoteListener remote)
         {
             /// 最近一次测试本机同时运行客户端服务器16000+连接时，服务器拒绝连接。
             var re = await remote.ListenAsync(Create);
             Listen(remote);
+            this.监听状态.Content = $"收到连接";
+            server = re;
         }
 
         public static EchoTcp Create()
         {
-            return new EchoTcp() { Post2ThreadScheduler = true };
+            return new EchoTcp();
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            client = new TestRemote();
+            client.UI = this;
+            client.ConnectAsync(new IPEndPoint(IPAddress.Loopback, 54321));
+            result1.Content = $"连接成功";
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            server.Send(new TestPacket1());
+        }
+
+        private async void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            var resp = await client.SendSafeAwait<TestPacket2>(new TestPacket2() { Value = 100 });
+            if (resp.Value == 100)
+            {
+                result1.Content = $"Rpc测试成功";
+            }
+            else
+            {
+                result1.Content = $"Rpc测试失败";
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            client.Client.Shutdown(SocketShutdown.Receive);
+        }
+    }
+}
+
+public class TestRemote : TcpRemote
+{
+    public MainWindow UI { get; internal set; }
+    protected async override ValueTask<object> OnReceive(short cmd, int messageID, object message)
+    {
+        switch (message)
+        {
+            case TestPacket1 packet1:
+                UI.result1.Content = $"收到{nameof(TestPacket1)}";
+                return new TestPacket2 { Value = packet1.Value };
+            case TestPacket2 packet2:
+                UI.result1.Content = $"收到{nameof(TestPacket2)}";
+                return null;
+            default:
+                break;
+        }
+        return null;
     }
 }
