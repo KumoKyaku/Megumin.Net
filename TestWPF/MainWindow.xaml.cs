ef91abae382d2dc4d87490230965d00384016182
云却 <479813005@qq.com>
2020-11-23 14:27:15 +0800

    修复命名空间

--- TestWPF/MainWindow.xaml.cs
+++ TestWPF/MainWindow.xaml.cs
@@ -1,22 +1,11 @@
 ﻿using Megumin.Remote;
-using Megumin.Remote.Echo;
+using Megumin.Remote.Simple;
 using Megumin.Remote.Test;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using TestWPF;
 
 namespace TestWPF
