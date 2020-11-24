143e8a9495885f36d023c23ff639b71c8e294c1d
云却 <479813005@qq.com>
2020-11-24 17:07:18 +0800

    测试程序界面布局调整

--- TestWPF/MainWindow.xaml.cs
+++ TestWPF/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ namespace TestWPF
     public partial class MainWindow : Window
     {
         private TestRemote client;
-        private EchoTcp server;
+        private TestRemote server;
 
         public MainWindow()
         {
@@ -27,53 +27,53 @@ namespace TestWPF
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             TcpRemoteListener listener = new TcpRemoteListener(54321);
             Listen(listener);
-            this.监听状态.Content = $"开始监听";
+            this.Serverlog.Content += $"\n 开始监听";
         }
 
         private async void Listen(TcpRemoteListener remote)
         {
             /// 最近一次测试本机同时运行客户端服务器16000+连接时，服务器拒绝连接。
-            var re = await remote.ListenAsync(Create);
+            var accept = await remote.ListenAsync(Create);
             Listen(remote);
-            this.监听状态.Content = $"收到连接";
-            server = re;
+            this.Serverlog.Content += $"\n 收到连接";
+            server = accept;
         }
 
-        public static EchoTcp Create()
+        public TestRemote Create()
         {
-            return new EchoTcp();
+            return new TestRemote() { log = this.Serverlog };
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void CreateClient(object sender, RoutedEventArgs e)
         {
             client = new TestRemote();
-            client.UI = this;
+            client.log = this.ClientLog;
             client.ConnectAsync(new IPEndPoint(IPAddress.Loopback, 54321));
-            result1.Content = $"连接成功";
+            ClientLog.Content += $"\n 连接成功";
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private void ServerSendMSG(object sender, RoutedEventArgs e)
         {
-            server.Send(new TestPacket1());
+            server.Send(new TestPacket1() { Value = 200 });
         }
 
-        private async void Button_Click_3(object sender, RoutedEventArgs e)
+        private async void TestRpc(object sender, RoutedEventArgs e)
         {
-            var resp = await client.SendSafeAwait<TestPacket2>(new TestPacket2() { Value = 100 });
-            if (resp.Value == 100)
+            var resp = await client.SendSafeAwait<TestPacket2>(new TestPacket2() { Value = 999 });
+            if (resp.Value == 999)
             {
-                result1.Content = $"Rpc测试成功";
+                ClientLog.Content += $"\n Rpc测试成功";
             }
             else
             {
-                result1.Content = $"Rpc测试失败";
+                ClientLog.Content += $"\n Rpc测试失败";
             }
         }
 
@@ -91,23 +91,34 @@ namespace TestWPF
         {
             client.Disconnect(true);
         }
+
+        private void ClientSend(object sender, RoutedEventArgs e)
+        {
+            client.Send(new TestPacket1() { Value = 100 }); ;
+        }
+
+        private void ClearLog(object sender, RoutedEventArgs e)
+        {
+            ClientLog.Content = "";
+            Serverlog.Content = "";
+        }
     }
 }
 
 public class TestRemote : TcpRemote
 {
-    public MainWindow UI { get; internal set; }
+    public Label log { get; set; }
     protected async override ValueTask<object> OnReceive(short cmd, int messageID, object message)
     {
-        UI.Dispatcher.Invoke(() =>
+        log.Dispatcher.Invoke(() =>
         {
             switch (message)
             {
                 case TestPacket1 packet1:
-                    UI.result1.Content = $"收到{nameof(TestPacket1)}";
+                    log.Content += $"\n 收到{nameof(TestPacket1)} value:{packet1.Value}";
                     break;
                 case TestPacket2 packet2:
-                    UI.result1.Content = $"收到{nameof(TestPacket2)}";
+                    log.Content += $"\n 收到{nameof(TestPacket2)} value:{packet2.Value}";
                     break;
                 default:
                     break;
@@ -116,21 +127,17 @@ public class TestRemote : TcpRemote
 
         switch (message)
         {
-            case TestPacket1 packet1:
-                return new TestPacket2 { Value = packet1.Value };
             case TestPacket2 packet2:
-                return null;
-            default:
-                break;
+                return message;
         }
         return null;
     }
 
     protected override void PostDisconnect(SocketError error = SocketError.SocketError, ActiveOrPassive activeOrPassive = ActiveOrPassive.Passive)
     {
-        UI.Dispatcher.Invoke(() =>
+        log.Dispatcher.Invoke(() =>
         {
-            UI.result1.Content = $"网络已断开 \n {error} -- {activeOrPassive}";
+            log.Content += $"\n 网络已断开 \n {error} -- {activeOrPassive}";
         });
     }
 }
