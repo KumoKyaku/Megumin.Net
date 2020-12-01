99f1a50cab7825f85b854f44906512eb68593d68
云却 <479813005@qq.com>
2020-12-01 13:43:33 +0800

    重命名文件

--- UnitTest/UnitTestRemote2.cs
+++ UnitTest/UnitTestRemote2.cs
@@ -14,7 +14,7 @@ using System.Threading.Tasks;
 namespace UnitTest
 {
     [TestClass]
-    public class UnitUdpTest
+    public class UnitTestRemote2
     {
         private UdpRemote CreateUdp()
         {
