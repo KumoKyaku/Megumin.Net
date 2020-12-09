f7099688d84d4abf1f9c3bc98a4226bb43d75edf
云却 <479813005@qq.com>
2020-12-09 14:13:21 +0800

    释放测试

---
+++ SocketTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TestLife();
+
+            Console.WriteLine("start");
+            Console.ReadLine();
+            GC.Collect();
+            GC.Collect(0, GCCollectionMode.Forced);
+            GC.Collect(1, GCCollectionMode.Forced);
+            GC.Collect(2, GCCollectionMode.Forced);
+            Console.WriteLine("gc");
+            Console.ReadLine();
+        }
+
+        private static void TestLife()
+        {
+            byte[] cache = new byte[10000];
+            Socket socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+            SocketAsyncEventArgs e = new SocketAsyncEventArgs();
+            e.Completed += (sender, e) => { };
+            socket.Bind(new IPEndPoint(IPAddress.IPv6Any, 54321));
+            socket.Listen();
+            socket.AcceptAsync(e);
+            //socket.Close();
+        }
+    }
+}
