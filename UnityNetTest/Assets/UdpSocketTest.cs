8010ed4432eab0b04dcf231e3af483c2355b4196
云却 <479813005@qq.com>
2022-09-14 20:35:08 +0800

    unity测试

--- UnityNetTest/Assets/UdpSocketTest.cs
+++ UnityNetTest/Assets/UdpSocketTest.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 using System.Net.Sockets;
 using System;
 using System.Net;
+using TMPro;
+using System.Threading.Tasks;
 
 public class UdpSocketTest : MonoBehaviour
 {
@@ -35,17 +37,48 @@ public class UdpSocketTest : MonoBehaviour
         GetIP();
     }
 
+    
     public async void GetIP()
     {
-        var gip = await IPAddressExtension_A6F086FB3EE3403BB5033720C34DA414.GetGloablIPAsync();
+        var gip = await IPAddressExtension_A6F086FB3EE3403BB5033720C34DA414.GetWANIP();
         if (gip != null)
         {
             globalIP.text = gip.ToString();
             //globalPort.text = gip.Port.ToString();
+            ping = new Ping(gip.ToString());
+            System.Net.NetworkInformation.Ping ping2 = new System.Net.NetworkInformation.Ping();
+            Ping(gip, ping2);
         }
     }
 
+    private async void Ping(IPAddress gip, System.Net.NetworkInformation.Ping ping2)
+    {
+        var resp = await ping2.SendPingAsync(gip);
+        if (resp.Status == System.Net.NetworkInformation.IPStatus.Success)
+        {
+            PingText.text = resp.RoundtripTime.ToString();
+        }
+        else
+        {
+            PingText.text = resp.Status.ToString();
+        }
+        await Task.Delay(500);
+        Ping(gip, ping2);
+    }
+
+    public TextMeshProUGUI PingText;
+    private Ping ping;
+    //public void Update()
+    //{
+    //    if (ping != null && ping.isDone)
+    //    {
+    //        PingText.text = ping.time.ToString();
+    //    }
+    //}
+
     static byte[] conn = new byte[8];
+
+
     private void SendByte()
     {
         IPEndPoint target = new IPEndPoint(
