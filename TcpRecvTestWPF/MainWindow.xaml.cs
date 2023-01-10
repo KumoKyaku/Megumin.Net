30de0af489e1cdf24eb0991586a535502b9b8755
云却 <479813005@qq.com>
2023-01-10 13:45:35 +0800

    增加Tcp io测试

--- TcpRecvTestWPF/MainWindow.xaml.cs
+++ TcpRecvTestWPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,52 @@ namespace TcpRecvTestWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Socket server;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void Listen_Click(object sender, RoutedEventArgs e)
+        {
+            TcpListener tcpListener = new TcpListener(54321);
+            tcpListener.Start();
+            LogAppend("开始监听");
+            server = await tcpListener.AcceptSocketAsync();
+            server.ReceiveBufferSize = 4096;
+            LogAppend("收到连接");
+        }
+
+        public void LogAppend(string message)
+        {
+            Log.Dispatcher.Invoke(() =>
+            {
+                Log.Text += message + "\n";
+            });
+        }
+
+        const int buffercount = 1024 * 10000;
+        byte[] buffer = new byte[buffercount];
+        private void BeginRecv_Click(object sender, RoutedEventArgs e)
+        {
+            server.BeginReceive(buffer, 0, buffercount, SocketFlags.None, RecvCallback, buffer);
+        }
+
+        private void RecvCallback(IAsyncResult ar)
+        {
+            int recvCount = server.EndReceive(ar);
+            byte[] buffer = ar.AsyncState as byte[];
+            LogAppend($"接收 {buffer[0]} {ar.IsCompleted} {recvCount}");
+
+        }
+
+        private void ClearLog_Click(object sender, RoutedEventArgs e)
+        {
+            Log.Dispatcher.Invoke(() =>
+            {
+                Log.Text = null;
+            });
         }
     }
 }
--- TcpSendTestWPF/MainWindow.xaml.cs
+++ TcpSendTestWPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,76 @@ namespace TcpSendTestWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Socket client;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.Connect(new IPEndPoint(IPAddress.Loopback, 54321));
+            client.Blocking = true;
+            client.SendBufferSize = 1024;
+            LogAppend("连接成功");
+        }
+
+        public void LogAppend(string message)
+        {
+            Log.Dispatcher.Invoke(() =>
+            {
+                Log.Text += message + "\n";
+            });
+        }
+
+        const int buffercount = 1024 * 10000;
+        byte[] buffer = new byte[buffercount];
+        /// <summary>
+        /// Q：为什么明明接收端没有Recv，这里也不会阻塞？
+        /// A：足够大的情况，还是会阻塞的。这个大小时如何设置的？并不是ReceiveBufferSize。
+        /// 应该时IO缓冲大小。接收端收到数据，程序不从IO中取出，当IO满后，将不在能接收。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Send1000_Click(object sender, RoutedEventArgs e)
+        {
+            logcount++;
+            buffer[0] = logcount;
+            LogAppend($"Send1000_Click 开始发送 {logcount}");
+            var sendBytes = client.Send(buffer);
+            LogAppend($"Send1000_Click 发送字节{sendBytes}");
+        }
+
+        byte logcount = 0;
+        private void LogTest_Click(object sender, RoutedEventArgs e)
+        {
+            logcount++;
+            LogAppend($"LogTest_Click 显示Log {logcount}");
+        }
+
+        private void SendAsync_Click(object sender, RoutedEventArgs e)
+        {
+            logcount++;
+            buffer[0] = logcount;
+            SendAsync2();
+            LogAppend($"SendAsync_Click 发送调用结束");
+        }
+
+        private async void SendAsync2()
+        {
+            LogAppend($"SendAsync_Click 开始发送 {logcount}");
+            var sendBytes = await client.SendAsync(buffer);
+            LogAppend($"SendAsync_Click 发送字节{sendBytes}");
+        }
+
+        private void ClearLog_Click(object sender, RoutedEventArgs e)
+        {
+            Log.Dispatcher.Invoke(() =>
+            {
+                Log.Text = null;
+            });
         }
     }
 }
